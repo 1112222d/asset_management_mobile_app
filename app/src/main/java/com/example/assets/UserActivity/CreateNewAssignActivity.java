a2a61dd25c8bd11f6eac499c2563d1fc9c2d646e
J <baviet19@gmail.com>
2021-11-03 23:37:59 +0700

    update code + design new view for create new_assign_activity

--- app/src/main/java/com/example/assets/UserActivity/CreateNewAssignActivity.java
+++ app/src/main/java/com/example/assets/UserActivity/CreateNewAssignActivity.java
@@ -1,6 +1,7 @@
 package com.example.assets.UserActivity;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -22,11 +23,13 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.assets.Adapter.CustomListViewCategoryAdapter;
 import com.example.assets.Adapter.CustomListViewSearchAssetAdapter;
 import com.example.assets.Adapter.CustomListViewSearchUserAdapter;
 import com.example.assets.Adapter.CustomSpinnerAdapter;
 import com.example.assets.Adapter.CustomSpinnerAssignRequestAdapter;
 import com.example.assets.AdminActivity.AssignmentManagementActivity;
+import com.example.assets.AdminActivity.CreateNewAssetActivity;
 import com.example.assets.AlterDialog.MessageDialog;
 import com.example.assets.MainActivity;
 import com.example.assets.Model.Asset;
@@ -51,13 +54,20 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CreateNewAssignActivity extends AppCompatActivity {
-    TextView nameCate, tv_assignedDate;
-    Spinner ed_cate;
-    EditText ed_note;
-    CustomSpinnerAssignRequestAdapter customSpinnerAssignRequestAdapter;
+    TextView nameCate, tv_assignedDate,ed_cate,tv_StartDate,tv_DueDate;
+    ImageView add,refresh;
+    EditText ed_number;
     List<Category> categories;
     Category cateSelect;
     Button button_createAssign;
+    ListView lv_CateSelect;
+    List<Category> listCategoriesSelect;
+    Dialog dialog;
+    public static CustomListViewCategoryAdapter customListViewCategoryAdapter;
+    MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+    MaterialDatePicker materialDatePicker;
+    long startDate=0, dueDate=0;
+    final Calendar calendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,68 +75,116 @@ public class CreateNewAssignActivity extends AppCompatActivity {
         nameCate = findViewById(R.id.tv_nameAssign);
         tv_assignedDate = findViewById(R.id.tv_assignedDate);
         ed_cate = findViewById(R.id.ed_cate);
-        ed_note = findViewById(R.id.ed_note);
+        ed_number = findViewById(R.id.ed_number);
+        lv_CateSelect=findViewById(R.id.listview_CategorySelect);
+        add=findViewById(R.id.add_category);
+        refresh=findViewById(R.id.refresh_category);
+        tv_StartDate=findViewById(R.id.date_start);
+        tv_DueDate=findViewById(R.id.due_date);
         categories = new ArrayList<>();
+        listCategoriesSelect= new ArrayList<>();
         button_createAssign = findViewById(R.id.button_createAssign);
-        customSpinnerAssignRequestAdapter = new CustomSpinnerAssignRequestAdapter(categories, CreateNewAssignActivity.this);
-        ed_cate.setAdapter(customSpinnerAssignRequestAdapter);
-
+        customListViewCategoryAdapter=new CustomListViewCategoryAdapter(CreateNewAssignActivity.this,categories);
         tv_assignedDate.setText("Date: " + DateFormat.format("dd/MM/yyyy", new Date()).toString());
 
         loadCate();
-        ed_cate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        ed_cate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                cateSelect = (Category) customSpinnerAssignRequestAdapter.getItem(position);
-
+            public void onClick(View view) {
+                dialog = new Dialog(CreateNewAssignActivity.this);
+                dialog.setContentView(R.layout.dialog_department_spinner);
+                dialog.getWindow().setLayout(800, 1800);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                TextView title =dialog.findViewById(R.id.title);
+                ListView listView = dialog.findViewById(R.id.listView);
+                Button button = dialog.findViewById(R.id.create);
+                title.setText("Choose Category");
+                listView.setAdapter(customListViewCategoryAdapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        cateSelect = (Category) customListViewCategoryAdapter.getItem(position);
+                        Toast.makeText(CreateNewAssignActivity.this, "" + cateSelect.getName()+" "+cateSelect.getPrefix(), Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
             }
+        });
+        tv_StartDate.setOnClickListener(v->{
+            if(check())
+            {
+                MessageDialog.getInstance(CreateNewAssignActivity.this,"Warning","If you choose the date again, the category selected list will be removed").setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Clear list and set date again
+                    }
+                }).setPositiveButton("Cancel",null).show();
+            }
+        });
+        tv_DueDate.setOnClickListener(v->{
 
+        });
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onClick(View view) {
+                if(cateSelect!=null&&Integer.parseInt(ed_number.getText().toString())>0)
+                {
+                    //add category to list
+                    //remove category from categorySelect
+                }
             }
         });
         button_createAssign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ed_note.getText().toString().trim().equals(""))
-                {
-                    MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Note must not blank").show();
-                }else
-                {
-                    MainActivity.service.createAssignRequest(new AssignRequestEntity(cateSelect.getPrefix(),ed_note.getText().toString())).enqueue(new Callback<AssignRequestRespone>() {
-                        @Override
-                        public void onResponse(Call<AssignRequestRespone> call, Response<AssignRequestRespone> response) {
-                            if(response.code()==200)
-                            {
-                                MessageDialog.getInstance(CreateNewAssignActivity.this,"Success","Create assign request success").show();
-                            }else
-                            {
-                                MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Create assign request fail").show();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<AssignRequestRespone> call, Throwable t) {
-                            MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Create assign request fail").show();
-                        }
-                    });
-                }
+//                if(ed_note.getText().toString().trim().equals(""))
+//                {
+//                    MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Note must not blank").show();
+//                }else
+//                {
+//                    MainActivity.service.createAssignRequest(new AssignRequestEntity(cateSelect.getPrefix(),ed_note.getText().toString())).enqueue(new Callback<AssignRequestRespone>() {
+//                        @Override
+//                        public void onResponse(Call<AssignRequestRespone> call, Response<AssignRequestRespone> response) {
+//                            if(response.code()==200)
+//                            {
+//                                MessageDialog.getInstance(CreateNewAssignActivity.this,"Success","Create assign request success").show();
+//                            }else
+//                            {
+//                                MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Create assign request fail").show();
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void onFailure(Call<AssignRequestRespone> call, Throwable t) {
+//                            MessageDialog.getInstance(CreateNewAssignActivity.this,"Error","Create assign request fail").show();
+//                        }
+//                    });
+//                }
             }
         });
     }
 
+    private boolean check() {
+        if(listCategoriesSelect.size()!=0)
+        {
+            if(!tv_DueDate.getText().toString().equals("dd/MM/yyyy")&&!tv_StartDate.getText().toString().equals("dd/MM/yyyy"))
+            {
+                return true;
+            }else return false;
+        }else return false;
+    }
+
     private void loadCate() {
         MainActivity.service.getCategory().enqueue(new Callback<List<Category>>() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
                 if (response.code() == 200) {
                     categories.clear();
-                    categories.addAll(response.body());
-                    customSpinnerAssignRequestAdapter.notifyDataSetChanged();
-                    ed_cate.setSelection(0);
-                    cateSelect = (Category) customSpinnerAssignRequestAdapter.getItem(0);
+                    categories.addAll(response.body().stream().filter(
+                            x->listCategoriesSelect.stream().anyMatch(y->y.getPrefix()==x.getPrefix())
+                    ).collect(Collectors.toList()));
                 }
             }
 
@@ -136,4 +194,5 @@ public class CreateNewAssignActivity extends AppCompatActivity {
             }
         });
     }
+
 }
